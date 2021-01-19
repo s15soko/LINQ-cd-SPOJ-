e8531ea8b57ead1aadabf975555f5784e3e50da7
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-19 21:13:24 +0100

    Add spoj code (result 100)

--- LINQ_cd/Program.cs
+++ LINQ_cd/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQ_cd
 {
@@ -6,7 +7,42 @@ namespace LINQ_cd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string times = Console.ReadLine();
+
+            if(times.Length == 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            var splitted = times.Split(',');
+            int count = splitted.Count();
+
+            var query = splitted
+                .Select(s => s.Split(':'))
+                .Select(s => (int.Parse(s[0]) * 60) + int.Parse(s[1]));
+
+            double timestamp = query.Sum();
+            double averageInSeconds = query.Average();
+
+            //
+
+            short minutes = (short) Math.Floor(averageInSeconds / 60);
+            short seconds = (short) Math.Ceiling((averageInSeconds - (minutes * 60)));
+
+            short tHours = (short) Math.Floor((timestamp / 3600) % 24);
+            short tMinutes = (short) Math.Floor((timestamp / 60) % 60);
+            short tSeconds = (short) Math.Ceiling(timestamp % 60);
+
+            if(tHours > 0)
+            {
+                Console.WriteLine($"{count} {minutes:D0}:{seconds:D2} {tHours:D0}:{tMinutes:D2}:{tSeconds:D2}");
+            } 
+            else
+            {
+                Console.WriteLine($"{count} {minutes:D0}:{seconds:D2} {tMinutes:D0}:{tSeconds:D2}");
+            }
+            
         }
     }
 }
